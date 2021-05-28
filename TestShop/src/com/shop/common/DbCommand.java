9c30a13238c01e8cb7e12738272f43e764576b90
admin <admin@YD02-13>
2021-05-28 11:20:23 +0900

    11:20

---
+++ TestShop/src/com/shop/common/DbCommand.java
@@ -0,0 +1,9 @@
+package com.shop.common;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface DbCommand {
+
+	public String execute(HttpServletRequest request, HttpServletResponse response);
+}
